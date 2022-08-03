64a7bc60a0497608de62e68ee2779480d568fa3d
Keelan <keelanmatthews123@gmail.com>
2022-08-03 14:58:36 +0200

    first commit

--- Practical 1/Task 6/Snapshot.h
+++ Practical 1/Task 6/Snapshot.h
@@ -6,10 +6,11 @@ class Snapshot: public AuditableSnapshot {
 private:
     std::string _state;
     std::string _date;
-
-public:
     Snapshot(std::string state);
     std::string state() const;
     std::string date() const;
     std::string GetUsername() const;
+
+    friend class User;
 };
+
